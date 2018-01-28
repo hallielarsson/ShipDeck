7e7593f149090ec6b0f4519e22eaa7e6c4e036c8
halstedlarsson <halsted.larsson@gmail.com>
2018-01-28 08:22:35 -0500

    refactored test card data to see what it would look like with async / await

--- scripts/defineCards.js
+++ scripts/defineCards.js
@@ -1,50 +1,49 @@
-
+//Not in use yet -- this is just the desired data spec
 
 var baseCards = {
   "found" : {
     name: "It's Found You!",
-    description: "You can feel it breathing",
-    tags: ["danger", "monster"];
-    userdata: { found: 0, hid: 0},
-    effects: [
-      function (card, app, done){
-        app.worry(Math.ceil(Math.random() * 10));
-        card.name = "It's Found You?",
-        card.message("You can't handle this.");
-        done();
-      },
-      function(card, app, done){
-        app.worry(3);
-        var hid = card.get("hid");
-        var found = card.get("found");
-        if(Math.random() > (0.5 + found / (found + hid))) {
-          card.set("hid", hid + 1);
-          card.name = "... Or Maybe It Hasn't"
-          card.message("You'll never believe it's gone.");
-          card.resolved = true;
-          card.isFinished = true;
-        };
-        done();
-      }
-    ],
-  },
-  "find_health" : {
-    name: "Health Pack",
-    description: "You find a white box.",
-    tags: ["heal"],
-    userdata: {uses: 10},
-    effects: [
-      function (card, app, done){
-        app.heal(Math.ceil(Math.random() * 10));
-        if(card.get("uses") > 1 ){
-          app.message("")
-        }
-        card.message ("You feel a little better.");
-        card.resolved = true;
-        app.exhaust(card);
-        done();
-      },
-    ],
-  },
+    description: "You can hear it breathing from the other side of the door.",
+    tags: ["bad", "monster"];
 
+    userdata: { found: 0, hid: 0},
+
+    effects: async card, app => {
+      var worryAmount = Math.ceil(Math.random() * 5);
+      app.worry(worryAmount);
+      await app.confirm("You can't handle this.");
+      var hid = card.get("hid");
+      var found = card.get("found");
+      app.message("A noise from the corner distracts it.");
+      await app.confirm("You take advantage of the few seconds of respite...");
+      if(Math.random() < (found / (found + hid))) {
+        card.set("hid", hid + 1);
+        app.worry(2);
+        app.message("The sound of its footsteps grow muffled in the distance as it stalks away ...");
+        await app.confirm("... but you'll never believe it's gone.");
+      } else {
+        card.set("found", found + 1);
+        await app.message("It hears you move, and quickly turns around.")
+        await app.effect("monsterAttack");
+      }
+      return "done";
+    }
+  },
+  "bleeding" : {
+    name: "Open Wound",
+    description: "The wound on your leg isn't healing well",
+    tags: ["wound", "bad"],
+    userdata : { count: 0},
+    effects: async card, app => {
+      app.damage(1);
+      var count = card.get("count");
+      count++;
+      card.set("count", count);
+      if(count > 5){
+        await app.confirm("The loss of blood makes you weak.");
+        await app.addCard("faint");
+      }
+      return done;
+    }
+  }
 };
